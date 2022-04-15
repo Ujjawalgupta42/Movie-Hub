4427810c5763b9f8c58f0cd6bfe2917743a700a9
Ujjawal Gupta <68456980+Ujjawalgupta42@users.noreply.github.com>
2022-04-15 22:28:53 +0530

    Updates

---
+++ precache-manifest.d3cc789c547bb3ce8865830405ec26ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e96de4f87fc1db9f4479a828df98f64f",
+    "url": "//Movie-Hub/index.html"
+  },
+  {
+    "revision": "1ffed6fbcc34008fc56f",
+    "url": "//Movie-Hub/static/css/main.778f4730.chunk.css"
+  },
+  {
+    "revision": "bd84b3c03cf4f4d2996b",
+    "url": "//Movie-Hub/static/js/2.e98fa1b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//Movie-Hub/static/js/2.e98fa1b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ffed6fbcc34008fc56f",
+    "url": "//Movie-Hub/static/js/main.f50c9b1e.chunk.js"
+  },
+  {
+    "revision": "4715902b75cd7325b1f2",
+    "url": "//Movie-Hub/static/js/runtime-main.4d95fadb.js"
+  },
+  {
+    "revision": "4e642236dcfe15f6d5a3952738199de5",
+    "url": "//Movie-Hub/static/media/bgm5.4e642236.png"
+  }
+]);
